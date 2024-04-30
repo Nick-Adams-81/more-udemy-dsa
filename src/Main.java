735d329455d12fd8cedf0acdd3f797590abcd854
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-04-30 15:24:22 -0700

    linkedlist insert method

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,36 @@ public class Main {
 
         System.out.println(newList.get(0).value);
 
+        newList.prepend(100);
+        newList.append(30);
+        newList.append(40);
+        newList.printList();
+        newList.printHead();
+        newList.printTail();
+        newList.printLength();
+
+        newList.set(0, 12);
+
+        newList.printHead();
+        newList.printTail();
+        newList.printLength();
+        newList.printList();
+        System.out.println("\n");
+
+        LinkedList myList = new LinkedList(1);
+        myList.append(2);
+        myList.append(3);
+        myList.prepend(0);
+        myList.printList();
+        myList.printHead();
+        myList.printTail();
+        myList.printLength();
+
+        myList.insert(2, 500);
+        myList.printList();
+        myList.printHead();
+        myList.printTail();
+        myList.printLength();
 
     }
 }
