c9cbd71c581072a9db672620e79a44c7f8f983de
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-05-07 20:36:27 -0700

    hash table constructor

---
+++ src/HashTable/Node.java
@@ -0,0 +1,12 @@
+package HashTable;
+
+public class Node {
+    public String key;
+    public int value;
+    Node next;
+
+    public Node(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
